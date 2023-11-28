43079268746bdce5be884b038b6d3db48d960331
LuTaoChen <alexanderloo625@gmail.com>
2023-11-28 17:32:18 +0800

    support yolov5 tpukernel postprocess fp16 implementation

--- pipeline/tpu_kernels.hpp
+++ pipeline/tpu_kernels.hpp
@@ -13,8 +13,8 @@
 using namespace std;
 
 typedef unsigned long long u64;
-#define MAX_YOLO_INPUT_NUM 3
-#define MAX_YOLO_ANCHOR_NUM 3
+#define MAX_YOLO_INPUT_NUM 8
+#define MAX_YOLO_ANCHOR_NUM 8
 typedef struct {
     u64 bottom_addr[MAX_YOLO_INPUT_NUM];
     u64 top_addr;
@@ -34,8 +34,6 @@ typedef struct {
 }__attribute__((packed)) tpu_kernel_api_yolov5NMS_t;
 
 
-#define MAX_YOLO_INPUT_NUM 3
-#define MAX_YOLO_ANCHOR_NUM 3
 typedef struct {
     u64 bottom_addr;
     u64 top_addr;
@@ -46,6 +44,7 @@ typedef struct {
     float confidence_threshold;
     int agnostic_nms;
     int max_hw;
+    u64 buffer_addr;
 }__attribute__((packed)) tpu_kernel_api_yolov5NMS_v2_t;
 
 
@@ -75,16 +74,15 @@ namespace bm {
         // memset(&start, 0, sizeof(start));
         // memset(&end, 0, sizeof(end));
         // bm_get_profile(ctx->handle, &start);
-
-        int out_len_max = 200 * 7;
+        int keep_top_k = 300;
+        int out_len_max = keep_top_k * 7;
         int batch_num = ctx->batchSize;
         float bias[18] = {10, 13, 16, 30, 33, 23, 30, 61, 62,
                           45, 59, 119, 116, 90, 156, 198, 373, 326};
         float downsample[3] = {8, 16, 32};
         int num_anchors = 3;
         float nms_threshold = 0.5;
-        float confidence_threshold = 0.1;
-        int keep_top_k = 200;
+        float confidence_threshold = 0.01;
         bm_device_mem_t in_dev_mem[outNum];
         bm_device_mem_t out_dev_mem;
         bm_device_mem_t detect_num_mem;
@@ -142,8 +140,8 @@ namespace bm {
         {
             api.anchor_scale[i] = downsample[i];
         }
-        api.clip_box = 0;
-        api.agnostic_nms = 1;
+        api.clip_box = 1;
+        api.agnostic_nms = 0;
 
         // std::chrono::steady_clock::time_point starts;
         // starts = std::chrono::steady_clock::now();
@@ -178,24 +176,20 @@ namespace bm {
         return 0;
     }
 
-    int yolov5_without_decode(int outNum, const TensorVec &outTensors, OutputType &postOut, ContextPtr ctx)
+    int yolov5_decode_f16(int outNum, const TensorVec &outTensors, OutputType &postOut, ContextPtr ctx)
     {
         tpu_kernel_function_t func_id;
         func_id = ctx->getKernelFuncId();
-
-        // bm_profile_t start, end;
-        // memset(&start, 0, sizeof(start));
-        // memset(&end, 0, sizeof(end));
-        // bm_get_profile(ctx->handle, &start);
-
-        int out_len_max = 200 * 7;
+        int keep_top_k = 300;
+        int out_len_max = keep_top_k * 7;
         int batch_num = ctx->batchSize;
-        int input_shape[3] = {1, 25200, 85};
+        float bias[18] = {10, 13, 16, 30, 33, 23, 30, 61, 62,
+                          45, 59, 119, 116, 90, 156, 198, 373, 326};
+        float downsample[3] = {8, 16, 32};
         int num_anchors = 3;
         float nms_threshold = 0.5;
-        float confidence_threshold = 0.001;
-        int keep_top_k = 200;
-        bm_device_mem_t in_dev_mem;
+        float confidence_threshold = 0.5;
+        bm_device_mem_t in_dev_mem[outNum];
         bm_device_mem_t out_dev_mem;
         bm_device_mem_t detect_num_mem;
         postOut.num = 2;
@@ -225,12 +219,115 @@ namespace bm {
         ret = bm_malloc_device_byte(ctx->handle, &detect_num_mem, batch_num * sizeof(int32_t));
         assert(BM_SUCCESS == ret);
 
+        // ============================ config ============================
+        tpu_kernel_api_yolov5NMS_t api;
+        for (int i = 0; i < outNum; i++)
+        {
+            in_dev_mem[i] = *outTensors[i]->get_device_mem();
+            api.bottom_addr[i] = bm_mem_get_device_addr(in_dev_mem[i]);
+        }
+        api.top_addr = bm_mem_get_device_addr(out_dev_mem);
+        api.detected_num_addr = bm_mem_get_device_addr(detect_num_mem);
+
+        api.input_num = outNum;
+        api.batch_num = batch_num;
+        for (int i = 0; i < outNum; ++i)
+        {
+            api.hw_shape[i][0] = outTensors[i]->shape(2);
+            api.hw_shape[i][1] = outTensors[i]->shape(3);
+        }
+
+        api.num_classes = outTensors[0]->shape(1) / num_anchors - 5;
+        api.num_boxes = num_anchors;
+        api.keep_top_k = keep_top_k;
+        api.nms_threshold = nms_threshold;
+        api.confidence_threshold = confidence_threshold;
+        memcpy((void *)api.bias, bias, 18 * sizeof(float));
+        for (int i = 0; i < outNum; ++i)
+        {
+            api.anchor_scale[i] = downsample[i];
+        }
+        api.clip_box = 0;
+        api.agnostic_nms = 1;
+
+        tpu_kernel_launch(ctx->handle, func_id, &api, sizeof(api));
+        bm_thread_sync(ctx->handle);
+
+        bm_memcpy_d2s_partial_offset(ctx->handle,
+                                     (void *)dt_num_data,
+                                     detect_num_mem,
+                                     batch_num * sizeof(int32_t),
+                                     0);
+        output_tensor.shape[2] = *dt_num_data;
+        if (*dt_num_data != 0) {
+            bm_memcpy_d2s_partial_offset(ctx->handle,
+                                         (void *)output_data,
+                                         out_dev_mem,
+                                         output_tensor.shape[2] * output_tensor.shape[3] * sizeof(float),
+                                         0);
+        }
+        return 0;
+    }
+
+    int yolov5_without_decode(int outNum, const TensorVec &outTensors, OutputType &postOut, ContextPtr ctx)
+    {
+        tpu_kernel_function_t func_id;
+        func_id = ctx->getKernelFuncId();
+
+        // bm_profile_t start, end;
+        // memset(&start, 0, sizeof(start));
+        // memset(&end, 0, sizeof(end));
+        // bm_get_profile(ctx->handle, &start);
+        int keep_top_k = 300;
+        int out_len_max = keep_top_k * 7;
+        int batch_num = ctx->batchSize;
+        int input_shape[3] = {1, 25200, 85};
+        int num_anchors = 3;
+        float nms_threshold = 0.5;
+        float confidence_threshold = 0.1;
+
+        bm_device_mem_t in_dev_mem;
+        bm_device_mem_t out_dev_mem;
+        bm_device_mem_t detect_num_mem;
+        bm_device_mem_t buffer_mem;
+        postOut.num = 2;
+        postOut.tensors = new tensor_data_t[2];
+
+        tensor_data_t &output_tensor = postOut.tensors[0];
+        tensor_data_t &detect_num = postOut.tensors[1];
+        auto output_data = new float[batch_num * keep_top_k * 7];
+        output_tensor.dtype = BM_FLOAT32;
+        output_tensor.shape[0] = 1;
+        output_tensor.shape[1] = 1;
+        output_tensor.shape[2] = 1;
+        output_tensor.shape[3] = 7;
+        output_tensor.dims = 4;
+        output_tensor.data = reinterpret_cast<uint8_t *>(output_data);
+
+        auto dt_num_data = new int32_t[batch_num];
+        detect_num.dtype = BM_UINT32;
+        detect_num.shape[0] = batch_num;
+        detect_num.dims = 1;
+        detect_num.data = reinterpret_cast<uint8_t *>(dt_num_data);
+
+        bm_status_t ret = BM_SUCCESS;
+
+        ret = bm_malloc_device_byte(ctx->handle, &out_dev_mem, out_len_max * sizeof(float));
+        assert(BM_SUCCESS == ret);
+        ret = bm_malloc_device_byte(ctx->handle, &detect_num_mem, batch_num * sizeof(int32_t));
+        assert(BM_SUCCESS == ret);
+        ret = bm_malloc_device_byte(ctx->handle, &buffer_mem, input_shape[1] * input_shape[2]);
+        assert(BM_SUCCESS == ret);
+
         //  ============================ config ============================
         tpu_kernel_api_yolov5NMS_v2_t api;
         in_dev_mem = *outTensors[0]->get_device_mem();
         api.bottom_addr = bm_mem_get_device_addr(in_dev_mem);
         api.top_addr = bm_mem_get_device_addr(out_dev_mem);
         api.detected_num_addr = bm_mem_get_device_addr(detect_num_mem);
+        api.buffer_addr = bm_mem_get_device_addr(buffer_mem);
+        // api.buffer_addr = 0;
+
         api.keep_top_k = keep_top_k;
         api.nms_threshold = nms_threshold;
         api.confidence_threshold = confidence_threshold;
@@ -298,7 +395,7 @@ namespace bm {
         int out_len_max = keep_top_k * 7;
         int batch_num = ctx->batchSize;
         float nms_threshold = 0.5;
-        float confidence_threshold = 0.1;
+        float confidence_threshold = 0.05;
         
         bm_device_mem_t in_dev_mem;
         bm_device_mem_t out_dev_mem;
@@ -367,6 +464,7 @@ namespace bm {
         { "tpu_kernel_api_yolov5_detect_out", yolov5_decode },
         { "tpu_kernel_api_yolov5_out_without_decode", yolov5_without_decode },
         { "tpu_kernel_api_yolov8_detect_out", yolov8_decode },
+        { "tpu_kernel_api_yolov5_detect_out_fp16", yolov5_decode_f16 },
     };
 
 }
